be860ab689fd2c49b21fef139af0ff16a67691e4
Brian O'Meara <omeara.brian@gmail.com>
2021-05-22 23:02:52 -0400

    using usethis functions to build up site quickly

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # flipped
-Various coin flipping models
+
+The goal of flipped is to explore models for coin flippings beyond the
+standard binomial.
+
+## Installation
+
+You can install the development version of this package from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("bomeara/flipped")
+```
+
+## Example
+
+This is a basic example:
+
+``` r
+library(flipped)
+nflips <- 100
+pheads <- 0.3
+nheads <- stats::rbinom(n=1, size=nflips, prob=pheads)
+```
--- tests/testthat/test-basic-functions.R
+++ tests/testthat/test-basic-functions.R
@@ -1,6 +1,8 @@
 test_that("flat linear works", {
   nheads <- 2
   nflips <- 3
-  expect_equal(dcoin_linear(nheads=nheads, nflips=nflips, preflip_prob=0.5, slope=0), stats::dbinom(x=2,size=3,prob=0.5, log=FALSE), tolerance=0.001)
-  expect_equal(dcoin_linear(nheads=nheads, nflips=nflips, preflip_prob=0.5, slope=0, log=TRUE), stats::dbinom(x=2,size=3,prob=0.5, log=TRUE), tolerance=0.001)
+  nflips <- 3
+  expect_equal(dcoin_linear(nheads=nheads, nflips=nflips, preflip_prob=0.5, slope=0), stats::dbinom(x=nheads,size=nflips,prob=0.5, log=FALSE), tolerance=0.001)
+  nflips <- 3
+  expect_equal(dcoin_linear(nheads=nheads, nflips=nflips, preflip_prob=0.5, slope=0, log=TRUE), stats::dbinom(x=nheads,size=nflips,prob=0.5, log=TRUE), tolerance=0.001)
 })
